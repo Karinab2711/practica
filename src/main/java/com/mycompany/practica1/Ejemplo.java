baf0b8dc8de1cba3295b4aa6d0c1145e5cf19a2c
karib <karib@LAPTOP-KARI>
2025-01-25 11:55:09 -0600

    agregar resta

--- src/main/java/com/mycompany/practica1/Ejemplo.java
+++ src/main/java/com/mycompany/practica1/Ejemplo.java
@@ -16,10 +16,12 @@ public class Ejemplo {
     System.out.println("Ingrese un nombre");
     numero2=scanner.nextInt();
     suma=numero1+numero2;
+    resta=numero1-numero2;
     System.out.println("El nombre es: "+ nombre);
     System.out.println("El primer número es: "+numero1);
     System.out.println("El segundo número es: "+numero2);
     System.out.println("El resultado de la suma: "+suma);
+    System.out.println("El resultado de la resta es: "+resta);
     scanner.close(); 
     }
 }
